1495869a3b732200130c83a2ba27af37fa0ae3f1
Shawn Axsom <axs221@gmail.com>
2015-12-14 16:44:19 -0500

    nothing special, looking at performance

--- server/app.js
+++ server/app.js
@@ -26,7 +26,8 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+//app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'pub')));
 
 app.use('/api/', routes);
 app.use('/api/articles', articles);
